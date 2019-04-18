79930351b2c98063ace78fb9181b1893905a08dc
Qun Li Huang <qunli02@gmail.com>
2019-04-18 19:28:54 +0000

    Done.

--- nyc_pigeon_organizer.rb
+++ nyc_pigeon_organizer.rb
@@ -1,3 +1,55 @@
 def nyc_pigeon_organizer(data)
   # write your code here!
+  name = []
+  data.each do |pige, life|
+    life.each do |stuff, nam|
+      name << nam
+    end
+  end
+  name.flatten!
+  name.uniq!
+  hash = {name[0] =>{}, name[1] =>{}, name[2] =>{}, name[3] =>{}, name[4] =>{}, name[5] =>{}, name[6] =>{}}
+  data.each do |pige, life|
+    life.each do |stuff, na|
+      na.each do |nam|
+        if nam == "Theo"
+          hash[nam][pige] =[]
+        elsif nam == "Peter Jr."
+          hash[nam][pige] =[]
+        elsif nam == "Lucky"
+          hash[nam][pige] =[]
+        elsif nam == "Ms. K"
+          hash[nam][pige] =[]
+        elsif nam == "Queenie"
+          hash[nam][pige] =[]
+        elsif nam == "Andrew"
+          hash[nam][pige] =[]
+        elsif nam == "Alex"
+          hash[nam][pige] =[]
+        end
+      end
+    end
+  end
+  data.each do |pige, life|
+    life.each do |stuff, na|
+      na.each do |nam|
+        if nam == "Theo"
+          hash[nam][pige] << stuff.to_s
+        elsif nam == "Peter Jr."
+          hash[nam][pige] << stuff.to_s
+        elsif nam == "Lucky"
+          hash[nam][pige] << stuff.to_s
+        elsif nam == "Ms. K"
+          hash[nam][pige] << stuff.to_s
+        elsif nam == "Queenie"
+          hash[nam][pige] << stuff.to_s
+        elsif nam == "Andrew"
+          hash[nam][pige] << stuff.to_s
+        elsif nam == "Alex"
+          hash[nam][pige] << stuff.to_s
+        end
+      end
+    end
+  end
+  return hash
 end
