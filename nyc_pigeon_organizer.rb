5e72b2397ff8d8e820a0ffd71999c2fc9be0542a
Qun Li Huang <qunli02@gmail.com>
2019-04-18 19:23:14 +0000

    Automatically backed up by Learn

--- nyc_pigeon_organizer.rb
+++ nyc_pigeon_organizer.rb
@@ -10,23 +10,24 @@ def nyc_pigeon_organizer(data)
   name.uniq!
   hash = {name[0] =>{}, name[1] =>{}, name[2] =>{}, name[3] =>{}, name[4] =>{}, name[5] =>{}, name[6] =>{}}
   data.each do |pige, life|
-    life.each do |stuff, nam|
-      if nam == "Theo"
-        hash[nam][pige] =[]
-      elsif nam == "Peter Jr."
-        hash[nam][pige] =[]
-      elsif nam == "Lucky"
-        hash[nam][pige] =[]
-      elsif nam == "Ms. K"
-        hash[nam][pige] =[]
-      elsif nam == "Queenie"
-        hash[nam][pige] =[]
-      elsif nam == "Andrew"
-        hash[nam][pige] =[]
-      elsif nam == "Alex"
-        hash[nam][pige] =[]
+    life.each do |stuff, na|
+      na.each do |nam|
+        if nam == "Theo"
+          hash[nam][pige] =[]
+        elsif nam == "Peter Jr."
+          hash[nam][pige] =[]
+        elsif nam == "Lucky"
+          hash[nam][pige] =[]
+        elsif nam == "Ms. K"
+          hash[nam][pige] =[]
+        elsif nam == "Queenie"
+          hash[nam][pige] =[]
+        elsif nam == "Andrew"
+          hash[nam][pige] =[]
+        elsif nam == "Alex"
+          hash[nam][pige] =[]
+        end
       end
-      return hash
     end
   end
   return hash
