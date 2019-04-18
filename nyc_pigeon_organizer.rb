63fe85ab3cf934adaff2354d41c3c490f187402b
Qun Li Huang <qunli02@gmail.com>
2019-04-18 19:15:49 +0000

    Automatically backed up by Learn

--- nyc_pigeon_organizer.rb
+++ nyc_pigeon_organizer.rb
@@ -8,7 +8,7 @@ def nyc_pigeon_organizer(data)
   end
   name.flatten!
   name.uniq!
-  hash = {name[0] =>{}, name[1] =>{}, name[2] =>{}, name[3] =>{}, {name[4] =>{}, name[5] =>{}, name[6] =>{}}
+  hash = {name[0] =>{}, name[1] =>{}, name[2] =>{}, name[3] =>{}, name[4] =>{}, name[5] =>{}, name[6] =>{}}
   data.each do |pige, life|
     life.each do |stuff, nam|
       if nam == "Theo"
