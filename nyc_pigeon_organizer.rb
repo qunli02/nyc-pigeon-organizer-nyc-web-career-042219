02c1ffc8c462570704481b4be718a284773d7a2e
Qun Li Huang <qunli02@gmail.com>
2019-04-18 18:30:20 +0000

    Automatically backed up by Learn

--- nyc_pigeon_organizer.rb
+++ nyc_pigeon_organizer.rb
@@ -1,4 +1,14 @@
 def nyc_pigeon_organizer(data)
   # write your code here!
-  key1 = data.keys
+  key1 = data.keys+
+  hash = {}
+  theo = {}
+  peter = {}
+  lucky = {}
+  data.each do |pige, life|
+    life.each |stuff , name|
+      x = 99
+    end
+  end
+  print x
 end
